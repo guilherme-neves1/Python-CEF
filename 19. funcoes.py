7b096b64a6077c04ba6c40fa1f799faa051b5625
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-05-02 10:35:58 -0300

    study functions in python

--- 19. funcoes.py
+++ 19. funcoes.py
@@ -53,5 +53,71 @@ def adiciona_numeros(x, y, z):
   
 somas = adiciona_numeros(1, 2, 3)
 print(somas)
+print()
+
+# COM 'return' O PRINT NÃO SERÁ NEM EXECUTADO
+def meu_laco():
+  for x in range(0, 25):
+    print(x)
+    if x == 5:
+      return 
+  print("Esta linha não será executada.")
+
+meu_laco()
+
+print()
+# COM 'break' O PRINT SERÁ EXECUTADO
+def meu_laco():
+  for x in range(0, 25):
+    print(x)
+    if x == 5:
+      break
+  print("Esta linha será executada.")
+
+meu_laco()
+
+print()
+# MAIN()
+# A inclusão de uma função main(), embora não seja necessária, pode estruturar nossos programas Python de uma maneira lógica que coloca os componentes mais importantes do programa em uma única função;
+def ola():
+  print("Olá mundo!")
+
+def main():
+  print("Esta é a função principal.")
+  ola()
+
+main()
+
+print()
+# No Python, '__main__' é o nome do escopo em que o código de nível superior será executado;
+# • Quando um programa é executado a partir da entrada padrão, de um script ou de um prompt interativo, seu __name__ é definido como '__main__'. 
+# • Por causa disso, existe uma convenção para usar a seguinte construção:
+# if __name__ == '__main__':
+# Código a ser executado quando este for o programa principal
+
+nome = str(input('Digite seu nome: '))
+
+def tem_vogal():
+  if set('aeiou').intersection(nome.lower()):
+    print('Seu nome contém vogais.')
+  else:
+    print('Seu nome não contém vogais.')
+
+def imprime_letras():
+  for letra in nome:
+    print(letra)
+
+def main():
+  tem_vogal()
+  imprime_letras()
+
+if __name__ == '__main__':
+  main()
+
+print()
+# DESAFIO
+
+
+
 
 
